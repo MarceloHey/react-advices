b280fc8eaff6d1aa256836431f8e830f39c72483
MarceloHey <marcelotoniolo.h@gmail.com>
2023-10-16 20:56:41 -0300

    Add fallback advice

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -4,8 +4,8 @@ const Advice = ({ advice }) => {
   return (
     advice
     && <div key={advice.id} className={styles.advice}>
-      <div className={styles.adviceCode}>Advice #{advice.id}</div>
-      <div className={styles.adviceText}>{advice.advice}</div>
+      <div className={styles.adviceCode}>Advice #{advice.id || 0}</div>
+      <div className={styles.adviceText}>{advice.advice || 'Here is an incredible advice for you ;)'}</div>
     </div>
 
   )
